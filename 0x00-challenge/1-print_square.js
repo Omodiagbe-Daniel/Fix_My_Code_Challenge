bdf293796c0177c64754a725123aa95016bd8503
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-09-19 05:40:28 +0000

    made correction

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-const size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2], 10);
 
 if (size > 0) {
     for (let i = 0 ; i < size ; i ++) {
